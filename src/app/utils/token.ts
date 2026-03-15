dc21f2d5e10112d9ded41e75237bd3fddc558332
asif12018 <asif.request@gmail.com>
2026-03-15 22:00:45 +0600

    refresh token function added

--- src/app/utils/token.ts
+++ src/app/utils/token.ts
@@ -30,12 +30,26 @@ const getRefreshToken = (payload: JwtPayload)=>{
 //set access token cookie
 
 const setAccessTokenCookie = (res: Response, token: string) => {
-    const maxAge = ms(envVars.ACCESS_TOKEN_EXPIRES_IN as any) as unknown as number;
+    const maxAge = ms(Number(envVars.ACCESS_TOKEN_EXPIRES_IN))
      CookieUtils.setCookie(res, 'accessToken', token, {
         httpOnly: true,
         secure: true,
         sameSite: 'none',
-        maxAge: maxAge
+        path:'/',
+        maxAge: Number(maxAge)
+     })
+}
+
+//set refresh token cookie
+
+const setRefreshTokenCookie = (res: Response, token: string) => {
+    const maxAge = ms(Number(envVars.REFRESH_TOKEN_EXPIRES_IN))
+     CookieUtils.setCookie(res, 'refreshToken', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none',
+        path:'/',
+        maxAge: Number(maxAge)
      })
 }
 
