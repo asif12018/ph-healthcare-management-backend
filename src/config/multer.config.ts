2847a537a836d99d3bf7409a37fc10e550de1105
asif12018 <asif.request@gmail.com>
2026-03-27 22:12:34 +0600

    cloudinary and multer added

---
+++ src/config/multer.config.ts
@@ -0,0 +1,29 @@
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { cloudinaryUpload } from "./cloudinary.config";
+import multer from "multer";
+
+
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinaryUpload,
+    params: async(req , file) =>{
+        const originalName = file.originalname;
+        const extension = originalName?.split(".").pop()?.toLowerCase();
+        const fileNameWithOutExtension = originalName?.split(".").slice(0, -1).join(".").toLowerCase().replace(/\s+/g, "-").replace(/[^a-z0-9\-]/g, "");
+
+        //creating unique name for file
+
+        const uniqueName = Math.random().toString(36).substring(2)+"-"+Date.now()+"-"+fileNameWithOutExtension;
+
+        const folder = extension === "pdf" ? "pdfs":"images";
+        return{
+            folder:`ph-healthcare/${folder}`,
+            public_id:uniqueName,
+            resource_type:"auto"
+        }
+       
+    }
+})
+
+
+export const multerUpload = multer({storage})
